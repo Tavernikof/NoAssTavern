ca48df06887df0fdab6d6d92a88597aaca9b82ea
sick_kind <sick_kind@proton.me>
2025-07-17 22:59:39 +0300

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,27 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath } from "node:url";
+import { viteSingleFile } from "vite-plugin-singlefile";
+
+console.log(process.env);
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "",
-  plugins: [react({
-    babel: {
-      parserOpts: {
-        plugins: ["decorators-legacy", "classProperties"],
+  plugins: [
+    react({
+      babel: {
+        parserOpts: {
+          plugins: ["decorators-legacy", "classProperties"],
+        },
       },
-    },
-  })],
+    }),
+    viteSingleFile(),
+  ],
   resolve: {
     alias: [
       { find: "src", replacement: fileURLToPath(new URL("./src", import.meta.url)) },
-    ]
+    ],
   },
   css: {
     preprocessorOptions: {
