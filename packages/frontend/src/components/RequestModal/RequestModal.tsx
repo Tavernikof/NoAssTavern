da2a0a7f35d5b6168d3f1356f8310335a5bd579c
sick_kind <sick_kind@proton.me>
2026-05-04 23:10:28 +0300

    fix(debug): split request log modal to 2 tabs

--- packages/frontend/src/components/RequestModal/RequestModal.tsx
+++ packages/frontend/src/components/RequestModal/RequestModal.tsx
@@ -3,6 +3,11 @@ import { requestStorage, RequestStorageItem } from "src/storages/RequestStorage.
 import style from "./RequestModal.module.scss";
 import Button from "src/components/Button";
 import { backendProviderDict } from "src/enums/BackendProvider.ts";
+import Tabs, { TabItem } from "src/components/Tabs";
+import PromptForm from "src/components/PromptEditorModal/components/PromptForm/PromptForm.tsx";
+import PromptCodeBlocksCounter
+  from "src/components/PromptEditorModal/components/PromptCodeBlocksCounter/PromptCodeBlocksCounter.tsx";
+import PromptCodeBlocks from "src/components/PromptEditorModal/components/PromptCodeBlocks/PromptCodeBlocks.tsx";
 
 type Props = {
   requestId: string;
@@ -17,32 +22,49 @@ const RequestModal: React.FC<Props> = (props) => {
     requestStorage.getItem(requestId).then(setRequest, () => setError(true));
   }, []);
 
+  const items = React.useMemo<TabItem[]>(() => {
+    if (!request) return [];
+    const { id, createdAt, provider, response: { inputTokens, outputTokens, url, request: requestParams } } = request;
+    const backendProvider = backendProviderDict.getById(provider);
+    const messages = backendProvider.class.getRequestMessages(requestParams);
+
+    return ([
+      {
+        key: "content",
+        title: "Content",
+        content: () => (
+          <div className={style.container}>
+            <Button onClick={() => console.log(request)}>Dump in console</Button>
+            <div><b>Id:</b> {id}</div>
+            <div><b>Time:</b> {createdAt.toLocaleString()}</div>
+            <div><b>Provider:</b> {provider}</div>
+            <div><b>Tokens count:</b>{inputTokens} / {outputTokens}</div>
+            <div>{url}</div>
+
+            {messages.map((message, i) => (
+              <div key={i} className={style.block}>
+                <div className={style.head}>{message.role}</div>
+                <div className={style.content}>{message.content}</div>
+              </div>
+            ))}
+          </div>
+        ),
+      },
+      {
+        key: "raw",
+        title: "Raw request",
+        content: () => <pre className={style.info}>{JSON.stringify(requestParams, null, 2)}</pre>,
+      },
+    ]);
+  }, [request]);
+
   if (error) return <div>Can not find request info</div>;
   if (!request) return null;
 
-  const { id, createdAt, provider, response: { inputTokens, outputTokens, url, request: requestParams } } = request;
-
-  const backendProvider = backendProviderDict.getById(provider);
-  const messages = backendProvider.class.getRequestMessages(requestParams);
-
   return (
-    <div className={style.container}>
-      <Button onClick={() => console.log(request)}>Dump in console</Button>
-      <div><b>Id:</b> {id}</div>
-      <div><b>Time:</b> {createdAt.toLocaleString()}</div>
-      <div><b>Provider:</b> {provider}</div>
-      <div><b>Tokens count:</b>{inputTokens} / {outputTokens}</div>
-      <div>{url}</div>
-
-      {messages.map((message, i) => (
-        <div key={i} className={style.block}>
-          <div className={style.head}>{message.role}</div>
-          <div className={style.content}>{message.content}</div>
-        </div>
-      ))}
-
-      <pre className={style.info}>{JSON.stringify(requestParams, null, 2)}</pre>
-    </div>
+    <Tabs
+      items={items}
+    />
   );
 };
 
